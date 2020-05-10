bd982b10170fc0d96df9fd644cced0961bc53270
Odin Standal <odin@odinodin.com>
2020-05-10 11:37:57 +0200

    Handle width and height

--- src/sine/core.cljs
+++ src/sine/core.cljs
@@ -21,7 +21,7 @@
   [:style {}
    (to-css {"#app" {:height "100vh"
                     :position "absolute"}
-            ".container" {:width "400px" :height "400px" :position "relative" :overflow "hidden"}
+            ".container" {:position "relative" :overflow "hidden"}
             ".button" {:padding "6px" :font-size "1.5rem" :cursor "pointer" :border "1px solid black" :display "inline-block"
                        :min-width "40px" :height "44px" :user-select "none"  :border-radius "2px" :background-color "lightgray"}
             ".letter" {:display "inline-block" :position "absolute" :font-size "2rem" :font-family "Helvetica Neue" :font-weight "bold"}})])
@@ -34,10 +34,10 @@
                  :color (or color "red")
                  :transform (str "rotate(" rotation ")")}} text])
 
-(defcomponent Main [{:keys [letters buttons]}]
+(defcomponent Main [{:keys [letters buttons width height]}]
   [:div
    (styling)
-   (into [:div {:className "container"}]
+   (into [:div {:className "container" :style {:width width :height height}}]
      (for [c letters]
        [Letter c]))
    (into [:div {:style {:display "flex"}}]
@@ -53,13 +53,13 @@
           [:button {:className "button"
                     :onclick (:dec-fn b)} "-"]])))])
 
-(def initial-state {:letters (seq "KODEMAvKER")
+(def initial-state {:letters (seq "KODEMAKER KODEMAKER KODEMAKER")
                     :tick 0
-                    :amplitude 10
-                    :y-offset 100
-                    :angle-speed 0.5
-                    :x-spacing 24
-                    :speed (/ 1 30)
+                    :amplitude 30
+                    :angle-speed 0.3
+                    :x-speed -10
+                    :x-spacing 26
+                    :speed (/ 1 8)
                     :rotation-multiplier 0.5})
 
 (defonce state (atom initial-state))
@@ -74,54 +74,57 @@
 (defn x-pos [{:keys [x width]}]
   (mod x width))
 
-(defn prepare [state]
+(defn prepare [state {:keys [width height]}]
   (let [{:keys [letters
                 tick
                 amplitude
                 angle-speed
-                y-offset
                 x-spacing
+                x-speed
                 rotation-multiplier]} @state]
-    {:buttons [{:text "Amplitude"
-                :inc-fn #(swap! state update :amplitude inc)
-                :dec-fn #(swap! state update :amplitude dec)}
-               {:text "Periode"
-                :inc-fn #(swap! state update :angle-speed (fn [angle-speed] (+ angle-speed 0.1)))
-                :dec-fn #(swap! state update :angle-speed (fn [angle-speed] (max (- angle-speed 0.1) 0)))}
-               {:text "Avstand"
-                :inc-fn #(swap! state update :x-spacing (fn [x-spacing] (+ x-spacing 1)))
-                :dec-fn #(swap! state update :x-spacing (fn [x-spacing] (max (- x-spacing 1) 0)))}
-               {:text "Fart"
-                :inc-fn #(swap! state update :speed (fn [x] (+ x 0.01)))
-                :dec-fn #(swap! state update :speed (fn [speed] (max (- speed 0.01) 0)))}
-               {:text "Rotasjon"
-                :on-click #(swap! state update :rotation-multiplier (fn [a] (if (= 0 a)
-                                                                              (:rotation-multiplier initial-state)
-                                                                              0)))}
-               {:text "Tilbakestill"
-                :on-click #(reset! state initial-state)}]
-     :letters (map-indexed (fn [idx letter]
-                             {:text letter
-                              :color (nth (cycle colors) idx)
-                              :top (-> (y-pos {:angle (+ tick (* idx angle-speed))
-                                               :amplitude amplitude
-                                               :y-offset y-offset}))
-                              :left (-> (x-pos {:x (+ (* tick 10) (* idx x-spacing))
-                                                :width 1200}))
-                              :rotation (-> (js/Math.cos (+ tick (* idx angle-speed)))
-                                            (* rotation-multiplier)
-                                            (radians-to-degree)
-                                            (str "deg"))})
-                letters)}))
+    (let [y-offset (/ height 2)
+          sentence-width (max width (* (+ (count letters) 1) x-spacing))]
+      {:buttons [{:text "Amplitude"
+                  :inc-fn #(swap! state update :amplitude inc)
+                  :dec-fn #(swap! state update :amplitude dec)}
+                 {:text "Periode"
+                  :inc-fn #(swap! state update :angle-speed (fn [angle-speed] (+ angle-speed 0.1)))
+                  :dec-fn #(swap! state update :angle-speed (fn [angle-speed] (max (- angle-speed 0.1) 0)))}
+                 {:text "Avstand"
+                  :inc-fn #(swap! state update :x-spacing (fn [x-spacing] (+ x-spacing 1)))
+                  :dec-fn #(swap! state update :x-spacing (fn [x-spacing] (max (- x-spacing 1) 0)))}
+                 {:text "Fart"
+                  :inc-fn #(swap! state update :speed (fn [x] (+ x 0.01)))
+                  :dec-fn #(swap! state update :speed (fn [speed] (max (- speed 0.01) 0)))}
+                 {:text "Rotasjon"
+                  :on-click #(swap! state update :rotation-multiplier (fn [a] (if (= 0 a)
+                                                                                (:rotation-multiplier initial-state)
+                                                                                0)))}
+                 {:text "Tilbakestill"
+                  :on-click #(reset! state initial-state)}]
+       :letters (map-indexed (fn [idx letter]
+                               {:text letter
+                                :color (nth (cycle colors) idx)
+                                :top (-> (y-pos {:angle (+ tick (* idx angle-speed))
+                                                 :amplitude amplitude
+                                                 :y-offset y-offset}))
+                                :left (-> (x-pos {:x (+ (* tick x-speed) (* idx x-spacing))
+                                                  :width sentence-width}))
+                                :rotation (-> (js/Math.cos (+ tick (* idx angle-speed)))
+                                              (* rotation-multiplier)
+                                              (radians-to-degree)
+                                              (str "deg"))})
+                  letters)
+       :width width
+       :height height})))
 
 
 (defn render []
-  (dumdom/render (Main (prepare state)) (js/document.getElementById "app")))
+  (dumdom/render (Main (prepare state {:width 600 :height 300})) (js/document.getElementById "app")))
 
 (defn render-loop []
   (render)
-  (swap! state update :tick #(do
-                               (+ % (:speed @state))))
+  (swap! state update :tick #(+ % (:speed @state)))
   (.requestAnimationFrame js/window render-loop))
 
 (defonce start
